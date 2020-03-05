24e00e9d6323dec5367ca4713c8d62089bf81eb9
Cthulhux <Cthulhux@noemail.net>
2020-03-05 09:49:02 +0000

    Init.
    FossilOrigin-Name: 589ec0c811528e953d6bd26790054187acac988684c23b0f5aaf55dd77025713

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instructions
+
+1. Install Windows. Sorry, no non-Windows support.
+2. Get this code.
+3. Get Go.
+4. `go get github.com/akavel/rsrc`.
+5. `go build -ldflags="-H windowsgui -s -w"`
+6. Done.
---
+++ dehexify.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "encoding/hex"
+    "log"
+    "regexp"
+    "strings"
+    
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+func RegexReplaceWithACallback(re *regexp.Regexp, str string, callback func([]string) string) string {
+    result := ""
+    lastIndex := 0
+    for _, v := range re.FindAllSubmatchIndex([]byte(str), -1) {
+        groups := []string{}
+        for i := 0; i < len(v); i += 2 {
+            groups = append(groups, str[v[i]:v[i+1]])
+        }
+        result += str[lastIndex:v[0]] + callback(groups)
+        lastIndex = v[1]
+    }
+    return result + str[lastIndex:]
+}
+
+func main() {
+	var inTE, outTE *walk.TextEdit
+
+	MainWindow{
+		Title:   "dehexify",
+		Size:    Size{800, 500},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &inTE},
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+			PushButton{
+				Text: "de-hexify",
+				OnClicked: func() {
+				    re := regexp.MustCompile("%([0-9A-F]{2})") // %3C, %FD, ...
+				    result := RegexReplaceWithACallback(re, inTE.Text(), func(groups []string) string {
+				        decoded, _ := hex.DecodeString(groups[1])
+                        return string(decoded)
+                    })
+                    
+                    // "+" usually stands for a space.
+					outTE.SetText(strings.ReplaceAll(result, "+", " "))
+				},
+			},
+			PushButton{
+				Text: "copy output",
+				OnClicked: func() {
+					if err := walk.Clipboard().SetText(outTE.Text()); err != nil {
+						log.Fatal("Copy error: ", err)
+					}
+				},
+			},
+		},
+	}.Run()
+}
