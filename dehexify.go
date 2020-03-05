26c47b5948ef75f22d6ad3c4cfe7fc5324f21b6a
Cthulhux <Cthulhux@noemail.net>
2020-03-05 09:51:01 +0000

    style
    FossilOrigin-Name: 05847338f943454e58b5139d1562dd93f78c4354b63b86e41f7a7ae97dd0a7f3

--- dehexify.go
+++ dehexify.go
@@ -6,8 +6,8 @@ import (
     "regexp"
     "strings"
     
-	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
+    "github.com/lxn/walk"
+    . "github.com/lxn/walk/declarative"
 )
 
 func RegexReplaceWithACallback(re *regexp.Regexp, str string, callback func([]string) string) string {
@@ -25,40 +25,40 @@ func RegexReplaceWithACallback(re *regexp.Regexp, str string, callback func([]st
 }
 
 func main() {
-	var inTE, outTE *walk.TextEdit
+    var inTE, outTE *walk.TextEdit
 
-	MainWindow{
-		Title:   "dehexify",
-		Size:    Size{800, 500},
-		Layout:  VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					TextEdit{AssignTo: &inTE},
-					TextEdit{AssignTo: &outTE, ReadOnly: true},
-				},
-			},
-			PushButton{
-				Text: "de-hexify",
-				OnClicked: func() {
-				    re := regexp.MustCompile("%([0-9A-F]{2})") // %3C, %FD, ...
-				    result := RegexReplaceWithACallback(re, inTE.Text(), func(groups []string) string {
-				        decoded, _ := hex.DecodeString(groups[1])
+    MainWindow{
+        Title:   "dehexify",
+        Size:    Size{800, 500},
+        Layout:  VBox{},
+        Children: []Widget{
+            HSplitter{
+                Children: []Widget{
+                    TextEdit{AssignTo: &inTE},
+                    TextEdit{AssignTo: &outTE, ReadOnly: true},
+                },
+            },
+            PushButton{
+                Text: "de-hexify",
+                OnClicked: func() {
+                    re := regexp.MustCompile("%([0-9A-F]{2})") // %3C, %FD, ...
+                    result := RegexReplaceWithACallback(re, inTE.Text(), func(groups []string) string {
+                        decoded, _ := hex.DecodeString(groups[1])
                         return string(decoded)
                     })
                     
                     // "+" usually stands for a space.
-					outTE.SetText(strings.ReplaceAll(result, "+", " "))
-				},
-			},
-			PushButton{
-				Text: "copy output",
-				OnClicked: func() {
-					if err := walk.Clipboard().SetText(outTE.Text()); err != nil {
-						log.Fatal("Copy error: ", err)
-					}
-				},
-			},
-		},
-	}.Run()
+                    outTE.SetText(strings.ReplaceAll(result, "+", " "))
+                },
+            },
+            PushButton{
+                Text: "copy output",
+                OnClicked: func() {
+                    if err := walk.Clipboard().SetText(outTE.Text()); err != nil {
+                        log.Fatal("Copy error: ", err)
+                    }
+                },
+            },
+        },
+    }.Run()
 }
